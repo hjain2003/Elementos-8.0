7560b2ac57977db48ab8c0f296b1e43626f0ea33
Harsh <hjain_be21@thapar.edu>
2023-01-12 22:30:53 +0530

    starting project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Landingpage from './components/Landingpage';
+
+function App() {
+  return (
+    <>
+    <Landingpage/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Landingpage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Landingpage = () => {
+  return (
+    <>
+    <div id="landingpage">
+        <h1>ELEMENTOS &#60;<span id="eight">8.0</span>&gt;</h1>
+    </div>
+    </>
+  )
+}
+
+export default Landingpage
