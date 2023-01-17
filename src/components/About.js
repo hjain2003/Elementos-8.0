c31426749adb1047f0d26ace91bca725dcd44a9b
Harsh <hjain_be21@thapar.edu>
2023-01-17 15:02:11 +0530

    added animations,aos,updated about section

--- src/components/About.js
+++ src/components/About.js
@@ -7,13 +7,16 @@ const About = () => {
     <div id="aboutmaindiv">
         
         <div id="abtevent">
-            <h1 id="abtheading">ABOUT THE EVENT</h1>
+            <h1 id="abtheading" data-aos="fade-right">ABOUT THE EVENT</h1>
             <br /><br /><br />
             <div id="AbtText_ufo">
-            <div id="AbtText">
+            <div id="AbtText" data-aos="flip-left">
             Proudly continuing the tradition of organising the Biggest Technical Fest in Thapar for over the past 5 years, IETE is back with another version upgrade : Elementos 8.O. A week-long series of fun-filled and brainstorming events, which are open for all the students in and across Thapar. Come in and Enjoy the intense events and staying productive at your best energy levels. Experience the unravelling of the big surprises ‼️
             </div>
-    {/* <img src={ufo_img} alt="" /> */}
+            <div id="ufoimage" data-aos="zoom-in-up">
+
+    <img src={ufo_img} alt="" />
+            </div>
             </div>
         </div>
     </div>
